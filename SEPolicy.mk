59736f4de38d58a4875c014d4eac37e910aace27
Michael Bestas <mkbestas@lineageos.org>
2019-09-26 02:16:53 +0300

    sepolicy: Update paths for new repository location
    Change-Id: Ibdaed7b3ff6463c682c65091ffbc82c36bfff348

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -1,5 +1,5 @@
 # Board specific SELinux policy variable definitions
-SEPOLICY_PATH:= device/qcom/sepolicy_vndr
+SEPOLICY_PATH:= device/qcom/sepolicy_vndr-legacy-um
 QSSI_SEPOLICY_PATH:= device/qcom/sepolicy
 SYS_ATTR_PROJECT_PATH := $(TOP)/device/qcom/sepolicy/generic/public/attribute
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
