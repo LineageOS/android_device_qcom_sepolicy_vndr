6f4cd84d0892b9774bcc10e6c4f111131a3c3821
Michael Bestas <mkbestas@lineageos.org>
2024-01-08 11:15:22 +0200

    bengal: Allow using legacy IMS rules
    Having both files in place causes a neverallow,
    so guard it behind a flag.
    
    Change-Id: Ic485e454b7f5d81b954ffffdd8743b3ca879cde1

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -49,6 +49,14 @@ ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
       BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
     endif
 
+    ifeq ($(TARGET_BOARD_PLATFORM),bengal)
+      ifeq ($(BOARD_USES_LEGACY_IMS_SEPOLICY),true)
+        BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/bengal/legacy-ims
+      else
+        BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/bengal/ims
+      endif
+    endif
+
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
       BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/test
       BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test
