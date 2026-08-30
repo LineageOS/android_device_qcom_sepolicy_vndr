0acbfce6046c37a9c14a52c84d3a3897a71f10c8
Alexander Koskovich <akoskovich@pm.me>
2026-08-30 16:18:58 -0400

    sepolicy_vndr: Add required policy for waipio on 6.6
    Change-Id: I53c306e41439913b5a6acf37c1852927f4e8a174

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -31,6 +31,12 @@ ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
        $(SEPOLICY_PATH)/generic/vendor/common/attribute \
        $(SEPOLICY_PATH)/qva/vendor/common
 
+    ifneq (,$(filter taro, $(TARGET_BOARD_PLATFORM)))
+      BOARD_VENDOR_SEPOLICY_DIRS += \
+         $(SEPOLICY_PATH)/generic/vendor/compat \
+         $(SEPOLICY_PATH)/qva/vendor/compat
+    endif
+
     ifeq ($(TARGET_SEPOLICY_DIR),)
       BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM)
       BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM)
